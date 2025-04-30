7263bf61a37763733b6cc64a7cf466bca6243dc6
Daekyue <howeorb@naver.com>
2025-04-30 15:31:34 +0900

    Fix : merge 충돌 해결

--- BACKEND/src/main/java/com/ssafy/fourcut/domain/image/repository/feedRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.fourcut.domain.image.repository;
-
-import com.ssafy.fourcut.domain.image.entity.Feed;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface feedRepository extends JpaRepository<Feed, Integer> {
-}
