af66d730df8084fb4e9727effb2bffd2ef7bc7ee
민승용 <sencroz@naver.com>
2025-05-07 16:23:18 +0900

    feat: QR 코드 스캔 및 파일 업로드 기능 추가
    - html5-qrcode 라이브러리 설치
    - QR 코드 스캔 컴포넌트 구현
    - 파일 업로드 컴포넌트 추가
    S12P31B208-121

--- FRONTEND/stella/src/app/page.tsx
+++ FRONTEND/stella/src/app/page.tsx
@@ -1,7 +1,11 @@
+import QrCode from "./components/main/QrCode";
+import AddFile from "./components/main/AddFile";
+
 export default function Home() {
   return (
     <div>
-      <h1>메인페이지</h1>
+      <QrCode />
+      <AddFile />
     </div>
   );
 }
