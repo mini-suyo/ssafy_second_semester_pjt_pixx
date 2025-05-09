d26c34b943a8e2336dd182a065237a467a4ed41f
rim.ssafy <rim.ssafy@gmail.com>
2025-05-09 13:54:21 +0900

    Feat: 카카오 id로 JWT 발급 시 발생하는 에러 예외 처리

--- BACKEND/src/main/java/com/ssafy/fourcut/domain/user/exception/UserNotFoundException.java
+++ BACKEND/src/main/java/com/ssafy/fourcut/domain/user/exception/UserNotFoundException.java
@@ -2,6 +2,10 @@ package com.ssafy.fourcut.domain.user.exception;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Integer userId) {
-        super("사용자를 찾을 수 없습니다.");
+        super("사용자를 찾을 수 없습니다: userId=" + userId);
+    }
+
+    public UserNotFoundException(Long kakaoId) {
+        super("등록된 사용자가 없습니다: kakaoId=" + kakaoId);
     }
 }
