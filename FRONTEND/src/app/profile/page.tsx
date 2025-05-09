7427c485a8b48cb989789c53ea52aa365a3bff6f
민승용 <sencroz@naver.com>
2025-05-09 10:13:27 +0900

    refactor: CSS 모듈로 인라인 스타일 마이그레이션하여 유지보수성 향상
    - 인라인 스타일을 CSS 모듈로 이전하여 스타일 관리 개선
    - 카카오 OAuth 콜백에 credentials 옵션 추가로 쿠키 처리 보장
    - 코드 구조화 및 가독성 향상을 위한 스타일 중앙화
    S12P31B208-123

--- FRONTEND/src/app/profile/page.tsx
+++ FRONTEND/src/app/profile/page.tsx
@@ -3,13 +3,16 @@
 import Inquiry from "@/components/login/Inquiry";
 import Logout from "@/components/login/Logout";
 import DeleteAccount from "@/components/login/DeleteAccount";
+import styles from "./page.module.css";
 
 export default function Page() {
   return (
-    <div className="flex flex-col gap-4 p-4">
-      <Inquiry />
-      <Logout />
-      <DeleteAccount />
+    <div className={styles.container}>
+      <div className={styles.buttonContainer}>
+        <Inquiry />
+        <Logout />
+        <DeleteAccount />
+      </div>
     </div>
   );
 }
