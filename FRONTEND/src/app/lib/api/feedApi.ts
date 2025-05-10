48956ef04addb16b2bf172e3b87c2c57a6a3929e
hyunhee <jhc03032@naver.com>
2025-05-10 23:58:02 +0900

    Feat : 피드 여러장 동시 삭제 api 구현 - S12P31B208-130

--- FRONTEND/src/app/lib/api/feedApi.ts
+++ FRONTEND/src/app/lib/api/feedApi.ts
@@ -1,7 +1,7 @@
 // app/lib/api/feedApi.ts
 
 import axiosInstance from "./axiosInstance";
-import { Feed } from "@/app/types/feed";
+import { Feed, DeleteFeedResponse } from "@/app/types/feed";
 
 //피드 목록 조회 API
 export const getFeeds = async (params: { type: number; page: number; size: number }): Promise<Feed[]> => {
@@ -13,4 +13,10 @@ export const getFeeds = async (params: { type: number; page: number; size: numbe
 export async function getFeedDetail(feedId: string) {
   const response = await axiosInstance.get(`/api/v1/feed/${feedId}`);
   return response.data.data;
+}
+
+// 피드 삭제 API
+export async function deleteFeed(payload: { imageList: number[] }): Promise<DeleteFeedResponse> {
+  const response = await axiosInstance.post("/api/v1/feed/delete", payload);
+  return response.data;
 }
