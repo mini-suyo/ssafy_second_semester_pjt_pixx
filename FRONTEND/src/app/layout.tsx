57caf58ac6aabc741144e67182711b84237a6c8f
hyunhee <jhc03032@naver.com>
2025-05-13 15:55:48 +0900

    feat: Google Tag Manager 스크립트 추가 및 axios 인스턴스 변경, 해시태그 수정 - S12P31B208-64

--- FRONTEND/src/app/layout.tsx
+++ FRONTEND/src/app/layout.tsx
@@ -7,6 +7,7 @@ import Header from "@/components/Header";
 import StarBackground from "@/components/StarBackground";
 import Navbar from "@/components/Navbar";
 import { usePathname } from "next/navigation";
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
@@ -14,7 +15,24 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html lang="ko">
+      <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TBPTTLQ3')`}
+        </Script>
+      </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TBPTTLQ3"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <div className="app-container">
           {!isWelcomePage && <Header />}
           <StarBackground />
