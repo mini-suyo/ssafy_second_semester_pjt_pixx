8a63a1ab2267c308984b203c8fe155621d95039b
hyunhee <jhc03032@naver.com>
2025-05-13 17:02:07 +0900

    Feat: LayoutClient 및 GTM 컴포넌트 추가, root layout 서버컴포넌트로 변환, providers 수정 및 의존성 업데이트

--- FRONTEND/src/app/layout.tsx
+++ FRONTEND/src/app/layout.tsx
@@ -1,44 +1,11 @@
-"use client";
-
-import type React from "react";
-import Providers from "./providers";
 import "./globals.css";
-import Header from "@/components/Header";
-import StarBackground from "@/components/StarBackground";
-import Navbar from "@/components/Navbar";
-import { usePathname } from "next/navigation";
-import Script from "next/script";
+import Providers from "./providers";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const isWelcomePage = pathname === "/welcome";
-
   return (
     <html lang="ko">
-      <head>
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TBPTTLQ3')`}
-        </Script>
-      </head>
       <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TBPTTLQ3"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        <div className="app-container">
-          {!isWelcomePage && <Header />}
-          <StarBackground />
-          <Providers className="main-content">{children}</Providers>
-          {!isWelcomePage && <Navbar />}
-        </div>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
