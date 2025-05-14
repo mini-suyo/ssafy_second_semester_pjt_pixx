1058f5e2c3fa4ddb23234c86cf1fd45b16d426b1
hyunhee <jhc03032@naver.com>
2025-05-14 09:41:12 +0900

    feat: Google Tag Manager 통합 및 해시태그 관련 코드 정리

--- FRONTEND/src/app/layout.tsx
+++ FRONTEND/src/app/layout.tsx
@@ -1,17 +1,33 @@
 // app/layout.tsx
 import "./globals.css";
 import Providers from "./providers";
-import GTMHead from "@/components/common/GTMHead";
-import GTMBody from "@/components/common/GTMBody";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <head>
-        <GTMHead />
+        {/* <!-- Google Tag Manager --> */}
+        <script>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TBPTTLQ3')`}
+          ;
+        </script>
+        {/* <!-- End Google Tag Manager --> */}
       </head>
       <body>
-        <GTMBody />
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TBPTTLQ3"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <Providers>{children}</Providers>
       </body>
     </html>
