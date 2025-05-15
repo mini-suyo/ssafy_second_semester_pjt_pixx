16456f95a45fbe290ebbcdba00393aad33914d25
민승용 <sencroz@naver.com>
2025-05-15 10:49:31 +0900

    refactor: 사용자 경험 개선을 위한 라우팅 및 스타일 업데이트
    - 인증된 사용자의 리다이렉트 경로를 /main 에서 /feed 로 변경
    - layout.tsx 에 메타데이터 추가 및 layout.server.tsx 에서 제거
    - 헤더와 네비바 스타일 피드백 반영
    - 코드 구조 개선을 위해 QrCode.tsx 의 handleQrResult 함수를 useEffect 내부로 이동
    S12P31B208-159

--- FRONTEND/src/app/layout.tsx
+++ FRONTEND/src/app/layout.tsx
@@ -3,11 +3,16 @@ import "./globals.css";
 import Providers from "./providers";
 import Script from "next/script";
 
+export const metadata = {
+  title: "Pixx",
+  description: "QR 기반 네컷 사진·GIF·영상을 자동으로 정리하고 추억을 간편하게 보관할 수 있는 웹 아카이브 플랫폼",
+  generator: "SSAFY B208",
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body>
-        {/* <!-- Google Tag Manager --> */}
         <Script
           id="gtm-script"
           strategy="afterInteractive"
