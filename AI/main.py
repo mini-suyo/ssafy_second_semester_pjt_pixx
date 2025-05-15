819012bd066b54689eb605052cd66f6b39e4c6d2
Daekyue <howeorb@naver.com>
2025-05-15 15:11:19 +0900

    Feat : FastAPI 초기 세팅

---
+++ AI/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "test입니당"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
